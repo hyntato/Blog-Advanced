095a4b4b9ff52b2619378629bd3d3192d45a60bf
hyn.tato <hyeyeonpark7183@gmail.com>
2022-09-04 16:17:29 +0900

    post

--- build/resources/main/schema.sql
+++ build/resources/main/schema.sql
@@ -1,8 +1,17 @@
-drop table if exists user;
+# drop table if exists user;
+# create table user (
+#                       user_id int not null auto_increment,
+#                       name varchar(128) not null,
+#                       email varchar(128) not null,
+#                       primary key (user_id)
+# ) default character set utf8 collate utf8_general_ci;
 
-create table user (
-                      id int not null auto_increment,
-                      name varchar(128) not null,
-                      email varchar(128) not null,
-                      primary key (id)
-) default character set utf8 collate utf8_general_ci;
+# drop table if exists post;
+# create table post (
+#                       post_id int not null auto_increment,
+#                       user_id int not null,
+#                       title varchar(128) not null,
+#                       content varchar(128) not null,
+#                       primary key (post_id),
+#                       foreign key (user_id) references user (user_id)
+# ) default character set utf8 collate utf8_general_ci;
