f4ae117b9c9f536b0e3fa4f2f2538dcd47a425b1
hyn.tato <hyeyeonpark7183@gmail.com>
2022-09-04 13:32:26 +0900

    init project
    프로젝트 기본 환경 세팅

--- src/main/java/com/hyntato/blog/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hyntato.blog.repository;
-
-import com.hyntato.blog.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- src/test/java/com/hyntato/blog/BlogApplicationTests.java
+++ src/test/java/com/hyntato/blog/BlogApplicationTests.java
@@ -1,50 +1,8 @@
 package com.hyntato.blog;
 
-import com.hyntato.blog.entity.User;
-import com.hyntato.blog.repository.UserRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.EnableMBeanExport;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class BlogApplicationTests {
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Test
-	@Order(1)
-	public void findUserById() {
-		Optional<User> user = userRepository.findById(1);
-		assertNotNull(user.get());
-	}
-
-	@Test
-	@Order(2)
-	public void findAllUsers() {
-		List<User> users = userRepository.findAll();
-		assertNotNull(users);
-	}
-
-	@Test
-	@Order(3)
-	public void createUser() {
-		User user = new User("hyntato", "hyntato@gamil.com");
-		User savedUser = userRepository.save(user);
-
-		User newUser = userRepository.findById(savedUser.getId()).get();
-		assertEquals("hyntato", newUser.getName());
-	}
 
 }
