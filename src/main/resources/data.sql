6060d6b7faf333f0bce6498301443bdcba8cf0c8
hyn.tato <hyeyeonpark7183@gmail.com>
2022-09-04 16:17:40 +0900

    post-test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
-insert into user (id, name, email) values (1, 'user1', 'user1@gmail.com');
-insert into user (id, name, email) values (2, 'user2', 'user2@gmail.com');
-insert into user (id, name, email) values (3, 'user3', 'user3@gmail.com');
-insert into user (id, name, email) values (4, 'user4', 'user4@gmail.com');
-insert into user (id, name, email) values (5, 'user5', 'user5@gmail.com');
+# insert into user (user_id, name, email) values (1, 'user1', 'user1@gmail.com');
+# insert into user (user_id, name, email) values (2, 'user2', 'user2@gmail.com');
+# insert into user (user_id, name, email) values (3, 'user3', 'user3@gmail.com');
+# insert into user (user_id, name, email) values (4, 'user4', 'user4@gmail.com');
+# insert into user (user_id, name, email) values (5, 'user5', 'user5@gmail.com');
+
+insert into post (post_id, user_id, title, content) values (1, 1, 'post1', 'content1');
+insert into post (post_id, user_id, title, content) values (2, 2, 'post2', 'content2');
+insert into post (post_id, user_id, title, content) values (3, 3, 'post3', 'content3');
+insert into post (post_id, user_id, title, content) values (4, 4, 'post4', 'content4');
+insert into post (post_id, user_id, title, content) values (5, 5, 'post5', 'content5');
